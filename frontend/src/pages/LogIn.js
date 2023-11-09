960b0834acc41284ae3716e000907023b224bc84
lolito <prof.lolito@gmail.com>
2023-11-09 11:15:13 +0200

    Front End Start

---
+++ frontend/src/pages/LogIn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LogIn() {
+  return (
+    <>
+    <h1>Logina</h1>
+    </>
+  )
+}
+
+export default LogIn
---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <>
+    <h1>Rejistarisha</h1>
+    </>
+  )
+}
+
+export default Register
